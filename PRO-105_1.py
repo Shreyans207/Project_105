fdf36e4bc77a6000ca2d88cf6e926bf169e95be7
Shreyans Gaur <75474973+Shreyans207@users.noreply.github.com>
2021-09-22 13:11:46 +0530

    Add files via upload

---
+++ PRO-105_1.py
@@ -0,0 +1,29 @@
+import math 
+import csv
+
+with open('PRO-105_2.csv')as f :
+    reader = csv.reader(f)
+    lists = list(reader)
+
+length = len(lists)
+
+def mean(lists) : 
+    half_mean = sum(total)
+    
+    mean = half_mean/length
+    return mean
+    
+# sigma_1 = []
+
+
+# for i in lists : 
+#     number = int(i[1])
+#     diff = number - mean(lists)
+#     squared = diff*diff
+#     sigma_1.append(squared)
+
+# summation = sum(sigma_1)
+# result = summation/length
+
+# st_deviation = math.sqrt(result)
+print(mean(lists))
