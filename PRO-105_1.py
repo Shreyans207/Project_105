131719e159647ef0ace112070c7788cf49b5efca
Shreyans Gaur <75474973+Shreyans207@users.noreply.github.com>
2021-09-28 09:42:35 +0530

    Update PRO-105_1.py

--- PRO-105_1.py
+++ PRO-105_1.py
@@ -8,22 +8,26 @@ with open('PRO-105_2.csv')as f :
 length = len(lists)
 
 def mean(lists) : 
+    total = 0
+    for i in lists : 
+        total = total + int(i[1])
+
     half_mean = sum(total)
     
     mean = half_mean/length
     return mean
     
-# sigma_1 = []
+sigma_1 = []
 
 
-# for i in lists : 
-#     number = int(i[1])
-#     diff = number - mean(lists)
-#     squared = diff*diff
-#     sigma_1.append(squared)
+for i in lists : 
+    number = int(i[1])
+    diff = number - mean(lists)
+    squared = diff*diff
+    sigma_1.append(squared)
 
-# summation = sum(sigma_1)
-# result = summation/length
+summation = sum(sigma_1)
+result = summation/length
 
-# st_deviation = math.sqrt(result)
+st_deviation = math.sqrt(result)
 print(mean(lists))
